b86432ea7b6d1829bd1d1b7355601fb714c13d47
Sunand-Vemavarapu <sunandvemavarapu@gmail.com>
2025-08-05 12:23:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express();
 // CORS configuration
 const corsOptions = {
   origin: [
+    "https://vinfrontned.onrender.com",
     "https://admin.vin2grow.in",
     "https://front3-f3cj.onrender.com",
     "https://vin2grow-latest-admin.onrender.com",
